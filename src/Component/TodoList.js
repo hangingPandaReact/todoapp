00c828246883cba2d529b03d23a1d8e5139d07ea
hangingPandaReact <ashutosh@hangingpanda.com>
2021-03-31 23:51:49 +0530

    Added logic for completion of todo app

--- src/Component/TodoList.js
+++ src/Component/TodoList.js
@@ -1,8 +1,22 @@
 
-const TodoList = () => (
-    <div>
-        TodoList
-    </div>
-);
+const TodoList = (props) => {
+    const {todoList, handleCheck} = props;
+    return (
+        <div>
+            {
+                todoList.map((list,index) => {
+                    const { name, isCompleted} = list
+                    return (
+                        <div>
+                            <input type="checkbox" checked={isCompleted} onChange={()=>{handleCheck(index)}} />
+                            {name}
+                            {isCompleted === true && <span style={{color:'red'}}>Completed</span>}
+                        </div>
+                    )
+                })
+            }
+        </div>
+    );
+}
 
 export default TodoList;
