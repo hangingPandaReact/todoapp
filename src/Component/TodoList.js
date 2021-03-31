e364814e656a41773dbe1a6fa5e9b92ec1f51737
hangingPandaReact <ashutosh@hangingpanda.com>
2021-03-31 22:43:48 +0530

    Added basic component

---
+++ src/Component/TodoList.js
@@ -0,0 +1,8 @@
+
+const TodoList = () => (
+    <div>
+        TodoList
+    </div>
+);
+
+export default TodoList;
---
+++ src/Component/index.js
@@ -0,0 +1,5 @@
+import TodoList from './TodoList'
+import AddTodoInput from './AddTodoInput'
+
+
+export { TodoList, AddTodoInput }
