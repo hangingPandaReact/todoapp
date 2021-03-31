63d7088eeaf13e6d93e2d76cfa7a1ae86b174f89
hangingPandaReact <ashutosh@hangingpanda.com>
2021-03-31 23:33:53 +0530

    Added logic to add todo from input on button click

--- src/Component/AddTodoInput.js
+++ src/Component/AddTodoInput.js
@@ -1,7 +1,18 @@
-const AddTodoInput = () => {
+const AddTodoInput = (props) => {
+    const { handleOnTodoName, handleAddTodo, todoName } = props;
     return (
         <div>
-           AddTodoInput 
+            <input 
+                type="text" 
+                value={todoName} 
+                placeholder="Enter the todo name" 
+                onChange={handleOnTodoName} 
+            />
+            <button 
+                onClick={handleAddTodo} 
+            >
+                Add Todo
+            </button>
         </div>
     )
 }
